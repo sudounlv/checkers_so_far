f11e61eabb2661a945f180b4850ed5b0a13b76fa
Adam Wushensky <awushensky@gmail.com>
2014-03-12 20:44:49 -0700

    added the service layer

--- checkers/src/main/java/edu/unlv/sudo/checkers/service/GameService.java
+++ checkers/src/main/java/edu/unlv/sudo/checkers/service/GameService.java
@@ -11,17 +11,31 @@ public interface GameService {
     /**
      * Join a game already in progress on a specified team.
      * @param gameId the ID of the game to join
-     * @param team the {@link edu.unlv.sudo.checkers.model.Team} to join
+     * @param team the {@link Team} to join
      * @param listener the {@link Listener} to handle callbacks
      */
     void joinGame(String gameId, Team team, Listener listener);
 
     /**
      * Create a new game.
-     * @param team the {@link edu.unlv.sudo.checkers.model.Team} to join
+     * @param team the {@link Team} to join
      * @param listener the {@link Listener} to handle callbacks
      */
     void newGame(Team team, Listener listener);
+
+    /**
+     * Move a piece in a game.
+     * @param game the new {@link Game} representing the moved piece
+     * @param listener the {@link Listener} to handle callbacks
+     */
+    void move(Game game, Listener listener);
+
+    /**
+     * Retrieve an update on the game from the server.
+     * @param gameId the ID of the game to update
+     * @param listener the {@link Listener} to handle callbacks
+     */
+    void update(String gameId, Listener listener);
 
 
     /**
--- checkers/src/main/java/edu/unlv/sudo/checkers/service/impl/GameServiceImpl.java
+++ checkers/src/main/java/edu/unlv/sudo/checkers/service/impl/GameServiceImpl.java
@@ -1,5 +1,16 @@
 package edu.unlv.sudo.checkers.service.impl;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import edu.unlv.sudo.checkers.CheckersBoard;
+import edu.unlv.sudo.checkers.model.Game;
 import edu.unlv.sudo.checkers.model.Team;
 import edu.unlv.sudo.checkers.service.GameService;
 
@@ -9,14 +20,90 @@ import edu.unlv.sudo.checkers.service.GameService;
 public class GameServiceImpl implements GameService {
 
     private static final String BASE_URL = "http://23.253.42.115:2020/checkers/game/";
+    private static final String CREATE_ENDPOINT = "create/";
+    private static final String JOIN_ENDPOINT = "join/";
+
+    private static final String URL_PARAM_DEVICE_ID = "deviceId";
+    private static final String URL_PARAM_TEAM = "color";
+
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public void joinGame(String gameId, Team team, Listener listener) {
-        //TODO: implement me!
+        final String url = BASE_URL + gameId + "/"
+                + JOIN_ENDPOINT + "/" + CheckersBoard.getDeviceUid();
+
+        issuePost(url, null, listener);
     }
 
     @Override
     public void newGame(Team team, Listener listener) {
+        final String url = BASE_URL + CREATE_ENDPOINT
+                + "?" + URL_PARAM_DEVICE_ID + "=" + CheckersBoard.getDeviceUid()
+                + "&" + URL_PARAM_TEAM + "=" + team.name();
+
+        issueGet(url, listener);
+    }
+
+    @Override
+    public void move(Game game, Listener listener) {
         //TODO: implement me!
+    }
+
+    @Override
+    public void update(String gameId, Listener listener) {
+        final String url = BASE_URL + gameId;
+
+        issueGet(url, listener);
+    }
+
+
+    /**
+     * Issue a GET request to the game server.
+     * @param url the URL to make the request to
+     * @param listener the callback listener
+     */
+    private void issueGet(final String url, final Listener listener) {
+        issuePost(url, null, listener);
+    }
+
+    /**
+     * Issue a POST request to the game server.
+     * @param url the URL to make the request to
+     * @param body the {@link org.json.JSONObject} body of the request
+     * @param listener the callback listener
+     */
+    private void issuePost(final String url, final JSONObject body, final Listener listener) {
+        final JsonObjectRequest request = new JsonObjectRequest(
+                url, body,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            listener.onGame(parseGame(response));
+                        } catch (IOException e) {
+                            listener.onError(e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        listener.onError(error);
+                    }
+                }
+        );
+
+        CheckersBoard.getRequestQueue().add(request);
+    }
+
+    /**
+     * Parse a {@link JSONObject} into a {@link edu.unlv.sudo.checkers.model.Game} object.
+     * @param jsonObject the {@link JSONObject} to parse
+     * @return the {@link edu.unlv.sudo.checkers.model.Game} result
+     * @throws IOException if unable to parse the JSON
+     */
+    private Game parseGame(final JSONObject jsonObject) throws IOException {
+        return mapper.readValue(jsonObject.toString(), Game.class);
     }
 }
